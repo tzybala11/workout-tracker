91d0214d333b171933cb7e17df90c9021d98bb97
Tristan Zybala <tzybala@usf.edu>
2025-12-06 21:34:46 -0500

    App.js file completed for the basic functionality of the app. Created WorkoutCard.js file and the Home & NewWorkout screens.

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import HomeScreen from './screens/HomeScreen';
+import NewWorkout from './screens/NewWorkout';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [workouts, setWorkouts] = useState([]);
+
+  const addWorkout = (newWorkout) => {
+    setWorkouts((current) => [
+      ...current,
+      {
+        id: Date.now().toString(),
+        ...newWorkout,
+      },
+    ]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Stack.Navigator>
+        <Stack.Screen name="Home">
+          {(props) => (
+            <HomeScreen {...props} workouts={workouts} />
+          )}
+        </Stack.Screen>
+
+        <Stack.Screen name="NewWorkout">
+          {(props) => (
+            <NewWorkout {...props} addWorkout={addWorkout} />
+          )}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
